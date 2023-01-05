cab86ff65554cea329aeda3651ae5f7711b8292a
Andres <andres.solanov.est@uets.edu.ec>
2023-01-05 12:41:10 -0500

    segundocommit

--- Pagcompra/src/app/app.module.ts
+++ Pagcompra/src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { UsererrorComponent } from './componentes/usererror/usererror.component';
 import { PagcompraComponent } from './componentes/pagcompra/pagcompra.component';
 import { PagcarritoComponent } from './componentes/pagcarrito/pagcarrito.component';
+
+const appRoutes:Routes=
+[
+  {
+    path: 'login' , component: LoginComponent
+  }
+
+]
 
 @NgModule({
   declarations: [
