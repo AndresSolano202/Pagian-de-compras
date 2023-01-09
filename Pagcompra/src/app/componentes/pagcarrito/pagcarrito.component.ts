9d7402897adeb247dd6d311301ab707288fd3f07
Andres <andres.solanov.est@uets.edu.ec>
2023-01-08 23:58:28 -0500

    ultimocommit

--- Pagcompra/src/app/componentes/pagcarrito/pagcarrito.component.ts
+++ Pagcompra/src/app/componentes/pagcarrito/pagcarrito.component.ts
@@ -10,6 +10,15 @@ export class PagcarritoComponent implements OnInit {
 
   constructor(private router: Router){}
 
+  p1: any;
+  p2: any;
+  p3: any;
+  p4: any;
+  p5: any;
+  p6: any;
+  p7: any;
+  p8: any;
+
   resultado:any;
   resultado2:any;
   resultado3:any;
@@ -21,25 +30,27 @@ export class PagcarritoComponent implements OnInit {
 
   ngOnInit():void{
     
-    
+    this.resultado=localStorage.getItem('valor1');
+    this.resultado2=localStorage.getItem('valor2');
+    this.resultado3=localStorage.getItem('valor3');
+    this.resultado4=localStorage.getItem('valor4');
+    this.resultado5=localStorage.getItem('valor5');
+    this.resultado6=localStorage.getItem('valor6');
+    this.resultado7=localStorage.getItem('valor7');
+    this.resultado8=localStorage.getItem('valor8');  
+
+    this.p1 = localStorage.getItem('p1')
+    this.p2 = localStorage.getItem('p2')
+    this.p3 = localStorage.getItem('p3')
+    this.p4 = localStorage.getItem('p4')
+    this.p5 = localStorage.getItem('p5')
+    this.p6 = localStorage.getItem('p6')
+    this.p7 = localStorage.getItem('p7')
+    this.p8 = localStorage.getItem('p8')
 
     //localStorage.clear()
   }
 
-
-  pagar()
-  {
-    this.resultado=localStorage.getItem('valor1')
-    this.resultado2=localStorage.getItem('valor2')
-    this.resultado3=localStorage.getItem('valor3')
-    this.resultado4=localStorage.getItem('valor4')
-    this.resultado5=localStorage.getItem('valor5')
-    this.resultado6=localStorage.getItem('valor6')
-    this.resultado7=localStorage.getItem('valor7')
-    this.resultado8=localStorage.getItem('valor8')
-    
-    
-  }
 
   home()
   {
--- Pagcompra/src/app/componentes/pagcompra/pagcompra.component.ts
+++ Pagcompra/src/app/componentes/pagcompra/pagcompra.component.ts
@@ -22,14 +22,16 @@ export class PagcompraComponent implements OnInit {
   p6: number=0;
   p7: number=0;
   p8: number=0;
-  precio1:number=75;
-  precio2:number=980;
-  precio3:number=980;
-  precio4:number=1299;
-  precio5:number=179;
-  precio6:number=1790;
-  precio7:number=1818.60;
-  precio8:number=1299;
+
+  precio1:number=0;
+  precio2:number=0;
+  precio3:number=0;
+  precio4:number=0;
+  precio5:number=0;
+  precio6:number=0;
+  precio7:number=0;
+  precio8:number=0;
+
   total:number=this.p1+this.p2+this.p3+this.p4+this.p5+this.p6+this.p7+this.p8;
   suma:number=0;
 
@@ -82,55 +84,96 @@ export class PagcompraComponent implements OnInit {
   produc1()
   {
     localStorage.setItem('valor1', this.precio1.toString())
-    
+    localStorage.setItem('p1', this.p1.toString())
   }
 
   produc2()
   {
     localStorage.setItem('valor2', this.precio2.toString())
+    localStorage.setItem('p2', this.p2.toString())
   }
 
   produc3()
   {
     localStorage.setItem('valor3', this.precio3.toString())
+    localStorage.setItem('p3', this.p3.toString())
   }
 
   produc4()
   {
     localStorage.setItem('valor4', this.precio4.toString())
+    localStorage.setItem('p4', this.p4.toString())
   }
 
   produc5()
   {
     localStorage.setItem('valor5', this.precio5.toString())
+    localStorage.setItem('p5', this.p5.toString())
   }
 
   produc6()
   {
     localStorage.setItem('valor6', this.precio6.toString())
+    localStorage.setItem('p6', this.p6.toString())
   }
 
   produc7()
   {
     localStorage.setItem('valor7', this.precio7.toString())
+    localStorage.setItem('p7', this.p7.toString())
   }
 
   produc8()
   {
     localStorage.setItem('valor8', this.precio8.toString())
+    localStorage.setItem('p8', this.p8.toString())
   }
 
 
+  nav()
+  {
+    this.router.navigate(['pagC'])
+    
+    localStorage.setItem('valor1', this.precio1.toString())
+    localStorage.setItem('p1', this.p1.toString())
+
+    localStorage.setItem('valor2', this.precio2.toString())
+    localStorage.setItem('p2', this.p2.toString())
+
+    localStorage.setItem('valor3', this.precio3.toString())
+    localStorage.setItem('p3', this.p3.toString())
+
+    localStorage.setItem('valor4', this.precio4.toString())
+    localStorage.setItem('p4', this.p4.toString())
+
+    localStorage.setItem('valor5', this.precio5.toString())
+    localStorage.setItem('p5', this.p5.toString())
+
+    localStorage.setItem('valor6', this.precio6.toString())
+    localStorage.setItem('p6', this.p6.toString())
+
+    localStorage.setItem('valor7', this.precio7.toString())
+    localStorage.setItem('p7', this.p7.toString())
+
+    localStorage.setItem('valor8', this.precio8.toString())
+    localStorage.setItem('p8', this.p8.toString())
+  }
+
+  log()
+  {
+    this.router.navigate([''])
+  }
+
   pagar()
   {
-    this.resultado=localStorage.getItem('valor1')
-    this.resultado2=localStorage.getItem('valor2')
-    this.resultado3=localStorage.getItem('valor3')
-    this.resultado4=localStorage.getItem('valor4')
-    this.resultado5=localStorage.getItem('valor5')
-    this.resultado6=localStorage.getItem('valor6')
-    this.resultado7=localStorage.getItem('valor7')
-    this.resultado8=localStorage.getItem('valor8')
+    this.resultado=localStorage.getItem('valor1');
+    this.resultado2=localStorage.getItem('valor2');
+    this.resultado3=localStorage.getItem('valor3');
+    this.resultado4=localStorage.getItem('valor4');
+    this.resultado5=localStorage.getItem('valor5');
+    this.resultado6=localStorage.getItem('valor6');
+    this.resultado7=localStorage.getItem('valor7');
+    this.resultado8=localStorage.getItem('valor8');
     
     this.r=Number(this.resultado)+Number(this.resultado2)+Number(this.resultado3)+Number(this.resultado4)+Number(this.resultado5)
     +Number(this.resultado6)+Number(this.resultado7)+Number(this.resultado8)
@@ -139,16 +182,6 @@ export class PagcompraComponent implements OnInit {
   datos()
   {
     this.user = localStorage.getItem('user');
-  }
-
-  nav()
-  {
-    this.router.navigate(['pagC'])
-  }
-
-  log()
-  {
-    this.router.navigate([''])
   }
 
   
